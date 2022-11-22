82586aafa6b50ff630ef67212f1e6aa58e7b4368
ChristianDeWinter <113032930+ChristianDeWinter@users.noreply.github.com>
2022-11-22 14:34:17 +0100

    Boomen kaart
    Kaart van locatie

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang='en'>
+  <head>
+    <meta charset='utf-8' />
+    <title>Points on a map</title>
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+    integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+    crossorigin=""/>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.css"/>
+    <link rel="stylesheet" type="text/css" href="style.css" /> 
+    <script src="https://unpkg.com/leaflet@1.9.3/dist/leaflet.js"
+    integrity="sha256-WBkoXOwTeyKclOHuWtc+i2uENFpDZ9YPdf5Hf+D7ewM="
+    crossorigin=""></script>
+<script src="https://cdn.jsdelivr.net/npm/leaflet.locatecontrol@0.78.0/dist/L.Control.Locate.min.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <div id="map"></div>
+    <script src="script.js"></script>
+  </body>
+</html>     
